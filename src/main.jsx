34dd84ff88e05258ed62b9c5a0788297a7c10258
Tabib Hassan <tabib@Tabibs-MacBook-Air.local>
2025-05-24 11:33:11 +0600

    Checkpoint1

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import './login.css'
+import './pages/login.css'
 import './signup.css'
 
 createRoot(document.getElementById('root')).render(
